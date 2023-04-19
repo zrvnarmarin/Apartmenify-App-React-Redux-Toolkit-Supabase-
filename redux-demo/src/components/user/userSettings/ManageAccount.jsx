739d2a12b2667974e9443d932307d348249e1d1e
Marin Zrvnar <76marin76@gmail.com>
2023-04-19 23:30:27 +0100

    user avatar and update email sections done

--- redux-demo/src/components/user/userSettings/ManageAccount.jsx
+++ redux-demo/src/components/user/userSettings/ManageAccount.jsx
@@ -5,18 +5,24 @@ import { selectUser } from '../../auth/usersSlice'
 const ManageAccount = () => {
   const user = useSelector(selectUser)
   const { user_metadata } = user
-  // console.log(user_metadata.avatar_url)
+  console.log(user_metadata.avatar_url)
+
   const [isEditNameClicked, setIsEditNameClicked] = useState(false)
   const toggleIsEditNameClicked = () => setIsEditNameClicked(prev => !prev)
 
+  const [isEditEmailClicked, setIsEditEmailClicked] = useState(false)
+  const toggleIsEditEmailClicked = () => setIsEditEmailClicked(prev => !prev)
+
   return (
     <div>
-      <div>
-        <h1 className='text-2xl'>Personal details</h1>
-        <h2 className='text-md'>Update your info and find out how it's used.</h2>
-        <p>{JSON.stringify(user_metadata)}</p>
-        <div>
-          <img src={''} />
+
+      <div className='flex flex-wrap flex-row items-center justify-between'>
+        <div className='flex flex-col gap-2'>
+          <h1 className='text-2xl'>Personal details</h1>
+          <h2 className='text-md'>Update your info and find out how it's used.</h2>
+        </div>
+        <div className='hover:bg-blue-100 p-4'>
+          <img className='rounded-full' src={user_metadata.avatar_url} />
         </div>
       </div>
 
@@ -27,10 +33,10 @@ const ManageAccount = () => {
           <input type="text" className='border-[1px] border-black p-2' />
           <label>Last name</label>
           <input type="text" className='border-[1px] border-black p-2' />
-          <button className='bg-blue-100 border-black border-[1px]' onClick={toggleIsEditNameClicked}>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditNameClicked}>
             Cancel
           </button>
-          <button className='bg-blue-100 border-black border-[1px]' onClick={toggleIsEditNameClicked}>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditNameClicked}>
             Save
           </button>
         </div>
@@ -38,7 +44,30 @@ const ManageAccount = () => {
         <div className='flex flex-row justify-between items-center'>
           <span>Name</span>
           <span>{user_metadata.name}</span>
-          <button className='bg-blue-100 border-black border-[1px]' onClick={toggleIsEditNameClicked}>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditNameClicked}>
+            Edit
+          </button>
+        </div>
+      }
+
+      { isEditEmailClicked
+      ?
+        <div>
+          <label>Email Address</label>
+          <input type="text" className='border-[1px] border-black p-2' />
+          <p>We'll send a verification link to your new email address – check your inbox.</p>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditEmailClicked}>
+            Cancel
+          </button>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditEmailClicked}>
+            Save
+          </button>
+        </div>
+      :
+        <div className='flex flex-row justify-between items-center'>
+          <span>Email Address <span className='text-white font-medium bg-green-500 rounded-md p-1'>Veriffied</span></span>
+          <span>{user_metadata.email}</span>
+          <button className='bg-blue-100 border-black border-[1px] p-2' onClick={toggleIsEditEmailClicked}>
             Edit
           </button>
         </div>
