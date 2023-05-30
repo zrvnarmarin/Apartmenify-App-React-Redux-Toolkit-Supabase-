6eb3854906cec34d811cf02e0b5c54cd81bb0789
zrvnarmarin <76marin76@gmail.com>
2023-05-30 03:47:35 +0200

    minor styling

--- redux-demo/src/components/admin/layout/SideMenu.jsx
+++ redux-demo/src/components/admin/layout/SideMenu.jsx
@@ -8,7 +8,10 @@ const SideMenu = ({ closeSideMenu }) => {
     <div className='font-poppins w-full h-[100%] bg-[#1f1f1f] fixed left-0 top-0 overflow-y-auto px-8 py-16 z-10'>
         <ul className='flex flex-col gap-16'>
             <li className='flex justify-end'>
-                <button aria-label='close_sidemenu' onClick={() => closeSideMenu()}>
+                <button 
+                    aria-label='close_sidemenu' 
+                    onClick={() => closeSideMenu()}
+                >
                     <img 
                         src={XButton} 
                         alt="close_sideMenu_button" 
@@ -18,41 +21,57 @@ const SideMenu = ({ closeSideMenu }) => {
                     />
                 </button>
             </li>
+
             <li className='flex flex-col items-center justify-center mb-16'>
                 <Link 
+                    onClick={closeSideMenu}
                     to="apartments" 
                     className='text-5xl hover:font-bold'
                 >
                     Apartmenify
                 </Link>
             </li>
+            
             <li className='tracking-wider flex flex-col items-center justify-center gap-16 text-3xl font-bold'>
                 <Link 
+                    onClick={closeSideMenu}
+                    to="apartments" 
+                    className='hover:underline hover:decoration-[#f0223d] decoration-2 underline-offset-[6px] duration-300'
+                >
+                    Apartments
+                </Link>
+                <Link 
+                    onClick={closeSideMenu}
                     to="facilities" 
                     className='hover:underline hover:decoration-[#f0223d] decoration-2 underline-offset-[6px] duration-300'
                 >
                     Facilities
                 </Link>
                 <Link 
+                    onClick={closeSideMenu}
                     to="reservations" 
                     className='hover:underline hover:decoration-[#f0223d] decoration-2 underline-offset-[6px] duration-300'
                 >
                     Reservations
                 </Link>
                 <Link 
+                    onClick={closeSideMenu}
                     to="stats" 
                     className='hover:underline hover:decoration-[#f0223d] decoration-2 underline-offset-[6px] duration-300'
                 >
                     Stats
                 </Link>
                 <Link 
+                    onClick={closeSideMenu}
                     to="users" 
                     className='hover:underline hover:decoration-[#f0223d] decoration-2 underline-offset-[6px] duration-300'
                 >
                     Users
                 </Link>
             </li>
+
             <hr />
+            
             <li><Logout /></li>
             
         </ul>
