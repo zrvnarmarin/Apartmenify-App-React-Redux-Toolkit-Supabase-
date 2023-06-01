afa06dd044b74f2e9d730814c76b7be521ca57f4
Marin Zrvnar <76marin76@gmail.com>
2023-06-01 16:22:01 +0200

    adding new wishlist restyled;

--- redux-demo/src/components/user/wishlists/Wishlists.jsx
+++ redux-demo/src/components/user/wishlists/Wishlists.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { selectUser, getAllWishlists } from '../../auth/usersSlice';
 
-const Saved = () => {
+const Wishlists = () => {
   const [isSavedApartmentsListShown, setIsSavedApartmentsListShown] = useState(false)
   const toggleSavedApartmentsList = () => setIsSavedApartmentsListShown(prev => !prev)
 
@@ -21,10 +21,9 @@ const Saved = () => {
   const setNewWishlist = e => dispatch(setWishlist(e.target.value))
 
   const savedApartments = useSelector(selectAllSavedApartments)
-  // console.log(savedApartments)
 
   const apartmentCountInEachWishlist = Object.entries(useSelector(numberOfSavedApartmentsInEachWishlist)) 
-  console.log(apartmentCountInEachWishlist)
+  // console.log(apartmentCountInEachWishlist)
 
   const submitFormHandler = (e) => {
     e.preventDefault()
@@ -52,19 +51,38 @@ const Saved = () => {
   }, [dispatch])
 
   return (
-    <div>
-      <div className='flex flex-row gap-2 items-center relative'>
+    <div className='flex flex-col gap-6 px-6 py-12 bg-[#1F1F1F]'>
+      <div className='flex flex-col ss:flex-row flex-wrap justify-between items-center gap-6'>
+        <h1 className='text-3xl font-semibold text-[#f4eff0] text-center ss:text-left'>Wishlists</h1>
+        <form onSubmit={submitFormHandler} className='flex flex-col ss:flex-row gap-4 w-full ss:w-fit'>
+          <input
+            type="text"
+            placeholder="Add new wishlist.."
+            className='bg-[#121212] text-[#f5f0f1] rounded-md px-6 py-2 outline-none focus:border-[1px] border-slate-500'
+            value={wishlist}
+            onChange={setNewWishlist}
+          />
+          <button className="px-6 py-2 my-4 ss:my-0 rounded-md text-lg font-medium bg-blue-100 text-[#f5eced] bg-gradient-to-r from-[#e8132f] to-[#fd3b54] drop-shadow-lg">
+            Add New Wishlist
+          </button>
+        </form>
+      </div>
 
-        <h1 className='text-2xl'>Saved</h1>
 
-        <button 
+
+      <div className='flex flex-row gap-2 items-center relative bg-[#1F1F1F]'>
+
+        {/* Gumb koji se stisne pa se na njemu prikaze yadnji uneseni wishlist */}
+        {/* <button 
           onClick={toggleSavedApartmentsList} 
           className='p-2 border-[1px] border-black bg-blue-100'
         >
           {allWishlists[allWishlists.length - 1].name}
-        </button>
+        </button> */}
 
-        <div>
+
+        {/* Prozor koji se otvori i prikaze sve wishlistove trenutne i delete i update opcije */}
+        {/* <div>
           { 
             isSavedApartmentsListShown && 
             <div className='flex flex-col gap-2 text-black p-4 rounded-lg border-[1px]
@@ -80,11 +98,13 @@ const Saved = () => {
               )}
             </div>
           }
-        </div>
+        </div> */}
 
-        <button onClick={toggleNewList} className='p-2 border-[1px] border-black bg-blue-100'>Create A List</button>
+        {/* Gumb koji otvori prozor u kojem se kreira novi wishlist */}
+        {/* <button onClick={toggleNewList} className='p-2 border-[1px] border-black bg-blue-100'>Create A List</button> */}
 
-        <div>
+        {/* Otvoreni prozor gdje se kreira novi wishlist */}
+        {/* <div>
           { 
             isCreateNewListShown && 
             <form onSubmit={submitFormHandler} className='flex flex-col gap-2 text-black p-4 rounded-lg border-[1px]
@@ -103,11 +123,14 @@ const Saved = () => {
               <button className='p-2 border-black border-[1px] bg-blue-100'>Create</button>
             </form>
           }
-        </div>
-{JSON.stringify(savedApartments)}
+        </div> */}
+
+
+
+{/* {JSON.stringify(savedApartments)} */}
       </div>
     </div>
   )
 }
 
-export default Saved
+export default Wishlists
