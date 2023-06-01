b72f4c4821e012f2686db9ae4ba420c50863b1e3
Marin Zrvnar <76marin76@gmail.com>
2023-06-01 16:36:32 +0200

    refactoring wishlist code;

--- redux-demo/src/components/user/wishlists/Wishlists.jsx
+++ redux-demo/src/components/user/wishlists/Wishlists.jsx
@@ -1,26 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import { addWishlist, selectAllWishlists, setWishlist, selectWishlist, resetWishlist, 
   getAllSavedApartments, getUser, selectAllSavedApartments, numberOfSavedApartmentsInEachWishlist 
 } from '../../auth/usersSlice'
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { selectUser, getAllWishlists } from '../../auth/usersSlice';
 
 const Wishlists = () => {
-  const [isSavedApartmentsListShown, setIsSavedApartmentsListShown] = useState(false)
-  const toggleSavedApartmentsList = () => setIsSavedApartmentsListShown(prev => !prev)
-
-  const [isCreateNewListShown, setIsCreateNewListShown] = useState(false)
-  const toggleNewList = () => setIsCreateNewListShown(prev => !prev)
-
   const { id: userId } = useSelector(selectUser)
   const wishlist = useSelector(selectWishlist)
   const allWishlists = useSelector(selectAllWishlists) 
 
   const dispatch = useDispatch()
   const setNewWishlist = e => dispatch(setWishlist(e.target.value))
-
-  const savedApartments = useSelector(selectAllSavedApartments)
+  // console.log(wishlist)
 
   const apartmentCountInEachWishlist = Object.entries(useSelector(numberOfSavedApartmentsInEachWishlist)) 
   // console.log(apartmentCountInEachWishlist)
@@ -34,20 +27,12 @@ const Wishlists = () => {
     }))
 
     dispatch(resetWishlist())
-
-    setIsCreateNewListShown(false)
+    toast.success(`Wishlist ${wishlist} has been added to database!`)
   }
 
   useEffect(() => {
     dispatch(getUser())
-    
     dispatch(getAllWishlists(userId))
-
-    dispatch(getAllSavedApartments({
-      userId: userId,
-      name: wishlist
-    }))
-    
   }, [dispatch])
 
   return (
@@ -68,66 +53,23 @@ const Wishlists = () => {
         </form>
       </div>
 
-
-
       <div className='flex flex-row gap-2 items-center relative bg-[#1F1F1F]'>
 
-        {/* Gumb koji se stisne pa se na njemu prikaze yadnji uneseni wishlist */}
-        {/* <button 
-          onClick={toggleSavedApartmentsList} 
-          className='p-2 border-[1px] border-black bg-blue-100'
-        >
-          {allWishlists[allWishlists.length - 1].name}
-        </button> */}
-
-
         {/* Prozor koji se otvori i prikaze sve wishlistove trenutne i delete i update opcije */}
-        {/* <div>
-          { 
-            isSavedApartmentsListShown && 
-            <div className='flex flex-col gap-2 text-black p-4 rounded-lg border-[1px]
-            border-black absolute top-[25%] left-20 bg-white '
-            >
-              {apartmentCountInEachWishlist.map(([key, value]) => 
-                <div key={key} className='flex row items-center gap-2'>
-                  <span>{key}:</span>
-                  <span className='rounded-full px-[6px] py-[1px] bg-slate-700 text-white'>{value}</span>
-                  <span className='p-2 bg-blue-100'>Update</span>
-                  <span className='p-2 bg-blue-100'>Delete</span>
-                </div>
-              )}
-            </div>
-          }
-        </div> */}
-
-        {/* Gumb koji otvori prozor u kojem se kreira novi wishlist */}
-        {/* <button onClick={toggleNewList} className='p-2 border-[1px] border-black bg-blue-100'>Create A List</button> */}
-
-        {/* Otvoreni prozor gdje se kreira novi wishlist */}
-        {/* <div>
-          { 
-            isCreateNewListShown && 
-            <form onSubmit={submitFormHandler} className='flex flex-col gap-2 text-black p-4 rounded-lg border-[1px]
-            border-black absolute top-[25%] left-[21%] bg-white '
-            >
-              <div className='flex flex-row items-center justify-between '>
-                <span>Create A New List</span>
-                <button onClick={toggleNewList} className='pr-1'>X</button>
+        <div>
+          <div className='flex flex-col gap-2 text-black p-4 rounded-lg border-[1px]
+          border-black absolute top-[25%] left-20 bg-white '
+          >
+            {apartmentCountInEachWishlist.map(([key, value]) => 
+              <div key={key} className='flex row items-center gap-2'>
+                <span>{key}:</span>
+                <span className='rounded-full px-[6px] py-[1px] bg-slate-700 text-white'>{value}</span>
+                <span className='p-2 bg-blue-100'>Update</span>
+                <span className='p-2 bg-blue-100'>Delete</span>
               </div>
-              <input 
-                type="text" 
-                placeholder='Name your new list' 
-                className='p-2 border-black border-[1px]'
-                onChange={setNewWishlist}
-              />
-              <button className='p-2 border-black border-[1px] bg-blue-100'>Create</button>
-            </form>
-          }
-        </div> */}
-
-
-
-{/* {JSON.stringify(savedApartments)} */}
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
