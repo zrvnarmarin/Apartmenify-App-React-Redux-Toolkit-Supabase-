56662995efa639d44c1d5239544041d78b5ab75f
zrvnarmarin <76marin76@gmail.com>
2023-06-28 23:00:16 +0200

    rating system now works on star mouseEnter event

--- redux-demo/src/components/user/ratings/Rating.jsx
+++ redux-demo/src/components/user/ratings/Rating.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import RatingStarFilled from '../../../assets/rating_icons/rating_star_filled_icon.png'
 import RatingStarEmpty from '../../../assets/rating_icons/rating_star_empty_icon.png'
 
-const ratingNumbers = [
+const array = [
     { id: 1, value: 1, isHovered: false },
     { id: 2, value: 2, isHovered: false },
     { id: 3, value: 3, isHovered: false },
@@ -11,63 +11,57 @@ const ratingNumbers = [
 ]
 
 const Rating = () => {
-
   return (
     <div className='flex flex-row items-center gap-4'>
-        {ratingNumbers.map(ratingNumber => 
-            <RatingStar key={ratingNumber.id} ratingNumber={ratingNumber.value} />
-        )}
+        <RatingStar />
     </div>
   )
 }
 
 export default Rating
 
-export const RatingStar = ({ ratingNumber }) => {
-    const ratingNumbers = [
-        { id: 1, value: 1, isHovered: false },
-        { id: 2, value: 2, isHovered: false },
-        { id: 3, value: 3, isHovered: false },
-        { id: 4, value: 4, isHovered: false },
-        { id: 5, value: 5, isHovered: false },
-    ]
-    const [isStarHovered, setIsStarHovered] = useState(false)
-    const [numbers, setRatingNumbers] = useState(ratingNumbers)
+export const RatingStar = () => {
+    const [ratingNumbers, setRatingNumbers] = useState(array)
 
     return (
-        <span className='bg-red-200'>
-            <img 
-                alt="rating_star" 
-                src={ isStarHovered ? RatingStarFilled : RatingStarEmpty } 
-                width={30} 
-                height={30} 
-                onMouseEnter={() => {
-                    setIsStarHovered(true)
-                    console.log(ratingNumber)
-
-                    setRatingNumbers(prev => {
-                        if (prev === undefined) return
-
-                        let hoveredStars = prev.slice(0, ratingNumber).map(star => ({ ...star, isHovered: true }));
-
-                        // it works on hover, but find a way to display it on component level
-                        console.log(hoveredStars)
-                        return hoveredStars
-                    })
-                }} 
-                onMouseLeave={() => {
-                    setIsStarHovered(false)
-                    console.log(ratingNumber)
-
-                    setRatingNumbers(prev => {
-                        console.log(prev)
-                        return prev
-                    })
-
-
-                }}
-            />
-            {ratingNumber}
-        </span>
+        <>
+            {ratingNumbers.map(ratingNumber =>
+                <span className='bg-red-200' key={ratingNumber.id}>
+                <img 
+                    alt="rating_star" 
+                    src={ ratingNumber.isHovered ? RatingStarFilled : RatingStarEmpty } 
+                    width={30} 
+                    height={30} 
+                    onMouseEnter={() => {
+                        // setIsStarHovered(true)
+                        console.log(ratingNumber.value)
+    
+                        setRatingNumbers(prev => {
+                            if (prev === undefined) return
+    
+                            let hoveredStars = prev.map(star => {
+                                star.value <= ratingNumber.value ? star.isHovered = true : star.isHovered = false
+                                return star
+                            })
+    
+                            console.log(hoveredStars)
+                            return hoveredStars
+                        })
+                    }} 
+                    onMouseLeave={() => {
+                        // setIsStarHovered(false)
+                        // console.log(ratingNumber)
+    
+                        setRatingNumbers(prev => {
+                            // console.log(prev)
+                            return prev
+                        })
+    
+    
+                    }}
+                />
+            </span>
+            )}
+        </>
     )
 }
